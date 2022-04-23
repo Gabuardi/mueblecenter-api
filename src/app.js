638f8af24a5950d10beee26b9b58729e1f1c8287
Josué Gabuardi <josuegabuardi@gmail.com>
2022-04-23 02:33:13 -0600

    Simulate fix

--- src/app.js
+++ src/app.js
@@ -2,37 +2,34 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
 
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/user/117/info', (req, res) => {
-    res.json({
-        id: 117,
-        name: 'Josué Gabuardi',
-        account: {
-            type: 'user',
-            creationDate: new Date('March 17, 2022 15:24:00')
-        },
-        payment: [
-            {
-                holder: 'JOSUE GABUARDI',
-                issuingNetworking: 'Visa',
-                cardNumber: '4485423059102178',
-            }
-        ],
-        addressBook: [
-            {
-                country: 'CR',
-                state: 'San José',
-                zip: 10098,
-                addressLine: 'Calle Matute, Casa planta alta'
-            }
-        ]
-    })
+// Después (Código Seguro)
+app.get('/user/info', (req, res) => {
+    res.json(getUserInfo(117))
 })
+
 
 app.get('/user/118/info', (req, res) => {
     res.json({
@@ -63,3 +60,29 @@ app.get('/user/118/info', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+function getUserInfo(id) {
+    return {
+        id: 117,
+            name: 'Josué Gabuardi',
+        account: {
+        type: 'user',
+            creationDate: new Date('March 17, 2022 15:24:00')
+    },
+        payment: [
+            {
+                holder: 'JOSUE GABUARDI',
+                issuingNetworking: 'Visa',
+                cardNumber: '4485423059102178',
+            }
+        ],
+            addressBook: [
+        {
+            country: 'CR',
+            state: 'San José',
+            zip: 10098,
+            addressLine: 'Calle Matute, Casa planta alta'
+        }
+    ]
+    }
+}
